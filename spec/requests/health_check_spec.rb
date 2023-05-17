68abe2659f36557bd0efc9cd85d4b2cbb2b5b2ce
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-05-17 22:19:33 +0700

    Group health check tests inside one describe block

--- spec/requests/health_check_spec.rb
+++ spec/requests/health_check_spec.rb
@@ -8,11 +8,7 @@ RSpec.describe 'HealthChecks', type: :request do
       get '/health_check'
       expect(response).to have_http_status(:success)
     end
-  end
-end
 
-RSpec.describe 'HealthChecks', type: :controller do
-  describe 'GET /health_check' do
     it { is_expected.to route(:get, '/health_check').to('health_check#health_check') }
   end
 end
