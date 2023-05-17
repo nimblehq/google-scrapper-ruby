f65f9d8a9f2a57cfeb780e7803e1b64fba833403
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-05-17 22:14:50 +0700

    Fix lint

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class HealthCheckController < ApplicationController
   def health_check
     render status: :ok, json: { message: 'OK' }
--- spec/requests/health_check_spec.rb
+++ spec/requests/health_check_spec.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "HealthChecks", type: :request do
-  describe "GET /health_check" do
-    it "returns http success" do
-      get "/health_check"
+RSpec.describe 'HealthChecks', type: :request do
+  describe 'GET /health_check' do
+    it 'returns http success' do
+      get '/health_check'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
 
 RSpec.describe 'HealthChecks', type: :controller do
