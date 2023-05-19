561ffc1ab78c31caab4a8b8cbf3ea170c491a7d3
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-05-19 11:30:28 +0700

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "rails/welcome#index"
+  root "home#index"
 
   get "/health_check", to: 'health_check#health_check', as: :rails_health_check
 end
