a22fac38ab817b686dd423653a3440a319098236
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-05-22 15:47:38 +0700

    Extract devise configs as a authenticable module for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 
-  # Other Devise configurations
-
-  validate :password_complexity, if: :password_required?
+  include Authenticable
 
   def password_complexity
     return if password.blank? || password.match?(/(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}/)
@@ -16,9 +10,4 @@ class User < ApplicationRecord
     errors.add(:password, 'must include at least one uppercase letter, one lowercase letter, and one digit')
   end
 
-  private
-
-  def password_required?
-    !persisted? || password.present? || password_confirmation.present?
-  end
 end
