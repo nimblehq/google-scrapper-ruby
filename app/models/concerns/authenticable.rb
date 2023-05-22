fadb21cb31bebeba1efe8725585516695979bc87
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-05-22 16:39:28 +0700

    Run password complexity validation on user create hook

--- app/models/concerns/authenticable.rb
+++ app/models/concerns/authenticable.rb
@@ -7,7 +7,5 @@ module Authenticable
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable and :omniauthable
     devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-
-    validate :password_complexity, if: :password_required?
   end
 end
