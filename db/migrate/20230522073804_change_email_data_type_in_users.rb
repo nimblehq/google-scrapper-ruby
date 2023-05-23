34a21081dade3bd8884a303198f4d472840b081d
topnimble <90754900+topnimble@users.noreply.github.com>
2023-05-23 10:11:17 +0700

    Enable and disable "citext" extension on migration

--- db/migrate/20230522073804_change_email_data_type_in_users.rb
+++ db/migrate/20230522073804_change_email_data_type_in_users.rb
@@ -1,9 +1,11 @@
 class ChangeEmailDataTypeInUsers < ActiveRecord::Migration[6.0]
   def up
+    enable_extension("citext")
     change_column :users, :email, :citext, default: "", null: false
   end
 
   def down
     change_column :users, :email, :string, default: "", null: false
+    disable_extension("citext")
   end
 end
