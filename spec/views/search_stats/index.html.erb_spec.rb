98b2513a2cdce9bee34c6b1f5b36fb1649e5931b
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-05-24 08:34:47 +0700

    Change test file search stats index view

--- spec/views/search_stats/index.html.erb_spec.rb
+++ spec/views/search_stats/index.html.erb_spec.rb
@@ -9,17 +9,17 @@ RSpec.describe 'search_stats/index', type: :view do
                keyword: 'Keyword',
                ad_count: 2,
                link_count: 3,
-               total_result_count: '',
+               total_result_count: 22,
                raw_response: 'MyText',
-               user_id: ''
+               user_id: 1
              ),
              SearchStat.create!(
                keyword: 'Keyword',
                ad_count: 2,
                link_count: 3,
-               total_result_count: '',
+               total_result_count: 22,
                raw_response: 'MyText',
-               user_id: ''
+               user_id: 1
              )
            ])
   end
@@ -28,10 +28,15 @@ RSpec.describe 'search_stats/index', type: :view do
     render
     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
     assert_select cell_selector, text: Regexp.new('Keyword'.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new('MyText'.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(1.to_s), count: 2
+  end
+
+  it 'renders a list of search_stats to check counts' do
+    render
+    cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
     assert_select cell_selector, text: Regexp.new(2.to_s), count: 2
     assert_select cell_selector, text: Regexp.new(3.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(''.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('MyText'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new(''.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new(22.to_s), count: 2
   end
 end
