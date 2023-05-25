3c4e6c08d0d47981314eb28ed952476b2da37169
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-05-25 16:06:02 +0700

    Remove wildcard hosts from prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,10 +100,3 @@ Rails.application.configure do
 
   config.hosts << ENV['DEPLOYED_HOSTS']
 end
-
-Rails.application.config.hosts = [
-  IPAddr.new("0.0.0.0/0"),        # All IPv4 addresses.
-  IPAddr.new("::/0"),             # All IPv6 addresses.
-  "localhost",                    # The localhost reserved domain.
-  ENV["DEPLOYED_HOSTS"]  # Additional comma-separated hosts for development.
-]
