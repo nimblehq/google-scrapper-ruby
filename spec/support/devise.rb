1abbda9075518c42eff2dd66ee0ef3d1e9cd9199
topnimble <90754900+topnimble@users.noreply.github.com>
2023-05-26 10:14:26 +0700

    Update helper

---
+++ spec/support/devise.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::ControllerHelpers, type: :helper
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+  config.include Devise::Test::IntegrationHelpers, type: :request
+end
