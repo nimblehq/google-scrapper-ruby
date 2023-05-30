6318643c91b3493f8752730f8fb84771e234a8a1
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-05-30 18:37:04 +0700

    [#10] Change search stat creation migration

--- db/migrate/20230523085326_create_search_stats.rb
+++ db/migrate/20230523085326_create_search_stats.rb
@@ -5,13 +5,11 @@ class CreateSearchStats < ActiveRecord::Migration[7.0]
       t.integer :ad_count, null: false, default: 0
       t.integer :link_count, null: false, default: 0
       t.bigint :total_result_count, null: false, default: 0
-      t.text :raw_response
-      t.bigint :user_id, null: false
+      t.text :raw_response, null: false
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
-
-    add_foreign_key :search_stats, :users, column: :user_id
     
     add_index :search_stats, :keyword
   end
