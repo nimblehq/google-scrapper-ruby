6ec2e4c63f48322433ec0bb86e1d55d718c7c35e
topnimble <90754900+topnimble@users.noreply.github.com>
2023-05-31 18:13:21 +0700

    Update tests

--- spec/requests/search_stats_spec.rb
+++ spec/requests/search_stats_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe 'Search Stats', type: :request do
   describe 'POST #create' do
     context 'given a valid file' do
-      it 'inserts keywords to the database' do
+      it 'inserts keywords to the database and redirects to search stat index' do
         user = Fabricate(:user)
         login_as_user user
 
@@ -19,16 +19,48 @@ RSpec.describe 'Search Stats', type: :request do
       end
     end
 
-    context 'given an invalid file' do
-      it 'does not insert keywords to the database' do
+    context 'given an invalid file data' do
+      it 'does not insert keywords to the database and raises an error' do
         user = Fabricate(:user)
         login_as_user user
 
-        params = { csv_file: fixture_file_upload('invalid.csv', 'text/csv') }
+        params = { csv_file: fixture_file_upload('invalid_data.csv', 'text/csv') }
 
         post :create, params: params
 
         expect(SearchStat.count).to eq(0)
+
+        expect(page).to raise_error(RuntimeError, 'Invalid file data')
+      end
+    end
+
+    context 'given an invalid file type' do
+      it 'does not insert keywords to the database and raises an error' do
+        user = Fabricate(:user)
+        login_as_user user
+
+        params = { csv_file: fixture_file_upload('invalid_type.txt', 'text/plain') }
+
+        post :create, params: params
+
+        expect(SearchStat.count).to eq(0)
+
+        expect(page).to raise_error(RuntimeError, 'Invalid file type')
+      end
+    end
+
+    context 'given too many keywords' do
+      it 'does not insert keywords to the database and raises an error' do
+        user = Fabricate(:user)
+        login_as_user user
+
+        params = { csv_file: fixture_file_upload('invalid_type.txt', 'text/plain') }
+
+        post :create, params: params
+
+        expect(SearchStat.count).to eq(0)
+
+        expect(page).to raise_error(RuntimeError, 'Too many keywords')
       end
     end
   end
