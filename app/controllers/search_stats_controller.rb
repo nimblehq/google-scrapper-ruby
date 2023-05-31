2004806ed38799cd54f1bd5f539e9f7b25e09ee0
topnimble <90754900+topnimble@users.noreply.github.com>
2023-05-31 18:23:19 +0700

    Remove manual file size check

--- app/controllers/search_stats_controller.rb
+++ app/controllers/search_stats_controller.rb
@@ -4,7 +4,6 @@ require 'csv'
 
 class SearchStatsController < ApplicationController
   ALLOWED_MIME_TYPE = 'text/csv'
-  MAXIMUM_FILE_SIZE_BYTES = 1000000
   MAXIMUM_KEYWORDS = 1000
 
   def new
@@ -17,7 +16,6 @@ class SearchStatsController < ApplicationController
     csv_file = params[:csv_file]
 
     raise 'Invalid file type' unless csv_file.content_type == ALLOWED_MIME_TYPE
-    raise 'File is too large' unless csv_file.size <= MAXIMUM_FILE_SIZE_BYTES
 
     csv_file_content = params[:csv_file].read
     keywords = CSV.parse(csv_file_content).flatten
