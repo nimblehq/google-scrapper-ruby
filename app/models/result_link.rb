08016c12affb62f845af3765e908559edff28f8c
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-06-01 18:25:38 +0700

    [#41] Sort models

--- app/models/result_link.rb
+++ app/models/result_link.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class ResultLink < ApplicationRecord
-  belongs_to :search_stat, inverse_of: :result_links
-
   enum link_type: { ads_top: 0, ads_page: 1, non_ads: 2 }
+
+  belongs_to :search_stat, inverse_of: :result_links
 
   validates :url, presence: true
 end
--- app/models/search_stat.rb
+++ app/models/search_stat.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class SearchStat < ApplicationRecord
+  has_many :result_links, inverse_of: :search_stat, dependent: :destroy
+  belongs_to :user
+
   validates :keyword, presence: true, length: { maximum: 255 }
   validates :raw_response, presence: true
-
-  belongs_to :user
-  has_many :result_links, inverse_of: :search_stat, dependent: :destroy
 end
