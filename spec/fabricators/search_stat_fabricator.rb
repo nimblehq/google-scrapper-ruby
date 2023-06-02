0445058466433648b3c70ff60c29a18007deab3c
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-06-02 09:56:14 +0700

    [#10] Create demo user to seed search stats for it

--- spec/fabricators/search_stat_fabricator.rb
+++ spec/fabricators/search_stat_fabricator.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
+demo_user = User.create(email: 'user@demo.com', password: 'Secret@11')
+
 Fabricator(:search_stat) do
   keyword { FFaker::Lorem.word }
   ad_count { rand(1..10) }
   link_count { rand(1..10) }
   total_result_count { rand(1..100) }
   raw_response { FFaker::HTMLIpsum.body }
-  user_id { Fabricate(:user).id }
+  user_id { demo_user.id }
 end
