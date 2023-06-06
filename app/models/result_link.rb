ad8a1a8163a030b7a14ce0b33a76f1a4c21f5b4c
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-06-06 16:36:39 +0700

    [#40] Remove ads_page link type from result link model

--- app/models/result_link.rb
+++ app/models/result_link.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ResultLink < ApplicationRecord
-  enum link_type: { ads_top: 0, ads_page: 1, non_ads: 2 }
+  enum link_type: { ads_top: 0, non_ads: 2 }
 
   belongs_to :search_stat, inverse_of: :result_links
 
