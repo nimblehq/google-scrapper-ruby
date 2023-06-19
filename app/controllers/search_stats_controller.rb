533a79b462593bb324c1ac3d24dfcbccbf68d62d
Md Mosharaf Hossan <mosharafkuet@gmail.com>
2023-06-19 18:54:16 +0700

    [#16] Use custom query class for keyword query

--- app/controllers/search_stats_controller.rb
+++ app/controllers/search_stats_controller.rb
@@ -3,16 +3,21 @@
 class SearchStatsController < ApplicationController
   # GET /search_stats
   def index
-    search_keyword = params[:search_keyword]
-
-    if search_keyword.present?
-      @pagy, @search_stats = pagy(current_user.search_stats.where(keyword: search_keyword))
-    else
-      @pagy, @search_stats = pagy(current_user.search_stats)
-    end
+    search_stats_query.call
+    @pagy, @search_stats = pagy(search_stats_query.keywords)
   end
 
   def show
     @search_stat = SearchStat.includes(:result_links).find(params[:id])
+  end
+
+  private
+
+  def search_stats_query
+    @search_stats_query ||= SearchStatsQuery.new(current_user.search_stats, permitted_params)
+  end
+
+  def permitted_params
+    params.permit(:keyword)
   end
 end
