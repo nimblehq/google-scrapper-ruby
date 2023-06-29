ab062f390d6edac2faf12dee927ffa93281c04be
Sang Huynh <sanghuynh20000@gmail.com>
2023-06-29 10:22:06 +0700

    Update precompile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  config.assets.compile = true
+
   # Configure Bullet gem to detect N+1 queries
   config.after_initialize do
     Bullet.enable        = true
@@ -85,6 +87,6 @@ Rails.application.configure do
     Bullet.rails_logger  = true
     Bullet.add_footer    = true
   end
-  
+
   config.hosts << ENV['DEPLOYED_HOST']
 end
